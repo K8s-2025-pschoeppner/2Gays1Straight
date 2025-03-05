5c2a31d6788ea018170e7da19c039237b0787a02
Vivian <115149501+vv-ab@users.noreply.github.com>
2025-03-05 13:08:53 +0100

    Update stateful.go

--- pkg/conditions/stateful.go
+++ pkg/conditions/stateful.go
@@ -62,3 +62,111 @@ func WithDifferentPodCount(minPods, total int) StatefulValidator {
 		return types.ErrStatefulValidatorContinue
 	}
 }
+
+func WithDifferenPodOnce(minPods int) StatefulValidator {
+	return func(ctx context.Context, store *flags.Store, r types.Request, client kubernetes.Interface) error {
+		if store == nil {
+			return fmt.Errorf("failed to write to nil store... again.")
+		}
+		podName := r.PodName
+		requestID := r.ID
+		pod, err := k8s.GetPod(ctx, client, podName, r.PodNamespace)
+		if err != nil {
+			return fmt.Errorf("failed to get pod %s/%s", podName, r.PodNamespace)
+		}
+		podTemplateHash := pod.Labels["pod-template-hash"]
+		mapKey := fmt.Sprintf("%s-%s", requestID, podTemplateHash)
+		podMap, exists := store.Get(mapKey)
+		if !exists {
+			m := make(map[string]int)
+			m[podName] = 1
+			store.Set(mapKey, m)
+			return types.ErrStatefulValidatorContinue
+		}
+		m, ok := podMap.(map[string]int)
+		if !ok {
+			return fmt.Errorf("failed to cast %q to map[string]int", reflect.TypeOf(podMap))
+		}
+		m[podName]++
+		store.Set(mapKey, m)
+		once := true
+		for _, v := range m {
+			if v > 1 {
+				once = false
+				break
+			}
+		}
+		if once && len(m) >= minPods {
+			return nil
+		}
+		return types.ErrStatefulValidatorContinue
+	}
+}
+
+func WithOnePodTwice() StatefulValidator {
+	return func(ctx context.Context, store *flags.Store, r types.Request, client kubernetes.Interface) error {
+		if store == nil {
+			return fmt.Errorf("failed to write to nil store... again.")
+		}
+		podName := r.PodName
+		requestID := r.ID
+		pod, err := k8s.GetPod(ctx, client, podName, r.PodNamespace)
+		if err != nil {
+			return fmt.Errorf("failed to get pod %s/%s", podName, r.PodNamespace)
+		}
+		podTemplateHash := pod.Labels["pod-template-hash"]
+		mapKey := fmt.Sprintf("%s-%s", requestID, podTemplateHash)
+		podCounter, exists := store.Get(mapKey)
+		if !exists {
+			c := 1
+			store.Set(mapKey, c)
+			return types.ErrStatefulValidatorContinue
+		}
+		c, ok := podCounter.(int)
+		if !ok {
+			return fmt.Errorf("failed to cast %q to int", reflect.TypeOf(podCounter))
+		}
+		c++
+		store.Set(mapKey, c)
+		if c == 2 {
+			return nil
+		}
+		return types.ErrStatefulValidatorContinue
+	}
+}
+
+func WithEveryNode() StatefulValidator {
+	return func(ctx context.Context, store *flags.Store, r types.Request, client kubernetes.Interface) error {
+		if store == nil {
+			return fmt.Errorf("failed to write to nil store... again.")
+		}
+		podName := r.PodName
+		requestID := r.ID
+		pod, err := k8s.GetPod(ctx, client, podName, r.PodNamespace)
+		if err != nil {
+			return fmt.Errorf("failed to get pod %s/%s", podName, r.PodNamespace)
+		}
+		node := pod.Spec.NodeName
+		nodeMap, exists := store.Get(requestID)
+		if !exists {
+			m := make(map[string]int)
+			m[node] = 1
+			store.Set(requestID, m)
+			return types.ErrStatefulValidatorContinue
+		}
+		m, ok := nodeMap.(map[string]int)
+		if !ok {
+			return fmt.Errorf("failed to cast %q to map[string]int", reflect.TypeOf(nodeMap))
+		}
+		m[node]++
+		store.Set(requestID, m)
+		nodeList, err := k8s.GetNodes(ctx, client)
+		if err != nil {
+			return fmt.Errorf("failed to get nodes")
+		}
+		if len(m) == len(nodeList.Items) {
+			return nil
+		}
+		return types.ErrStatefulValidatorContinue
+	}
+}
